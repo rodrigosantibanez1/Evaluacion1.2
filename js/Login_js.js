f86b872d682f1c80f3e8f2e598f6af79dee5fe42
caillanes <ca.illanes@duocuc.cl>
2023-05-17 23:33:36 -0400

    cambios index

--- js/Login_js.js
+++ js/Login_js.js
@@ -8,30 +8,30 @@ form.onsubmit = (e)=>{
   //Si el correo electrónico y la contraseña están en blanco, agregue la clase Shake y llame a la función especificada
   (eInput.value == "") ? eField.classList.add("shake", "error") : checkEmail();
   (pInput.value == "") ? pField.classList.add("shake", "error") : checkPass();
-  setTimeout(()=>{ //Eliminar la clase de agitación después de 500ms
+  setTimeout(()=>{ //Eliminar la clase de shake después de 500ms
     eField.classList.remove("shake");
     pField.classList.remove("shake");
   }, 500);
   eInput.onkeyup = ()=>{checkEmail();} //llamar a la función checkEmail en email input keyup
   pInput.onkeyup = ()=>{checkPass();} //llamar a la función checkPassword en pass input keyup
   function checkEmail(){ //checkEmail
-    let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/; //pattern for validate email
-    if(!eInput.value.match(pattern)){ //if pattern not matched then add error and remove valid class
+    let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/; //pattern para validar email
+    if(!eInput.value.match(pattern)){ //si el pattern no coincide, agrega el error y quita la clase valid
       eField.classList.add("error");
       eField.classList.remove("valid");
       let errorTxt = eField.querySelector(".error-txt");
-      //if email value is not empty then show please enter valid email else show Email can't be blank
+      //si email no esta vacio, muestra el mensaje: Correo ingresado no valido, si no que muestre el mensaje: El correo no puede estar vacio
       (eInput.value != "") ? errorTxt.innerText = "Correo ingresado no valido" : errorTxt.innerText = "El correo no puede estar vacio";
-    }else{ //if pattern matched then remove error and add valid class
+    }else{ //si pattern coincide, quita el error y agrega la clase valid
       eField.classList.remove("error");
       eField.classList.add("valid");
     }
   }
   function checkPass(){ //checkPass function
-    if(pInput.value == ""){ //if pass is empty then add error and remove valid class
+    if(pInput.value == ""){ //si pass esta vacio, agrega el error y remueve la clase valid
       pField.classList.add("error");
       pField.classList.remove("valid");
-    }else{ //if pass is empty then remove error and add valid class
+    }else{ //si pass esta vacio, remueve el error y agregac la clase valid
       pField.classList.remove("error");
       pField.classList.add("valid");
     }
