7e1652518cddf46a8f2d25f7b2b693e4f553801a
Diego <di.allende@duocuc.cl>
2023-05-18 13:43:52 -0400

    vf VE

--- Jquery/ventas_especiales.js
+++ Jquery/ventas_especiales.js
@@ -1,29 +1,30 @@
 $(document).ready(function() {
+  var container = $("#container");
+
+  function ajustarItemsPorFila() {
+    var windowWidth = $(window).width();
+    var itemsPorFila = windowWidth >= 576 ? 3 : 2; 
+
+    container.find(".col").removeClass("col-md-4 col-sm-6").addClass("col-md-" + (12 / itemsPorFila));
+  }
+
+  ajustarItemsPorFila();
+  $(window).resize(ajustarItemsPorFila);
+
   $.get("https://fakestoreapi.com/products", function(data) {
     $.each(data, function(i, item) {
-      var card = $("<div class='col' padding-bottom='5%'> "+
-                      "<div class='card' style='width: 18rem;'>"+
-                      "<img class='card-img-top' src='"+item.image+"'height='180' width='286px' alt='Card image cap'>"+
-                      "<div class='card-body'>"+
-                        "<h5 class='card-title'>"+item.title+"</h5>"+
-                        "<p class='card-text'>"+item.price+"</p>"+
-                        "<a href='#' class='btn btn-primary'>Go somewhere</a>"+
-                      "</div>"+
-                   " </div></div>"
-                  );
+      var card = $("<div class='col-md-4 col-sm-6'>  "+
+                    "<div class='card' style='width: 18rem;'>"+
+                    "<img class='card-img-top' src='"+item.image+"' height='180' width='286px' alt='Card image cap'>"+
+                    "<div class='card-body'>"+
+                      "<h5 class='card-title'>"+item.title+"</h5>"+
+                      "<p class='card-text'>"+item.price+"</p>"+
+                      "<a href='#' class='btn btn-primary' id='details'>Go somewhere</a>"+
+                    "</div>"+
+                  "</div></div>"
+                 );
 
-      card.click(function() {
-        var detalle = $("<div><h2>" + item.title + "</h2> <button class='close'>X</button><p><b>Descripción:</b> " +
-                        item.description + "</p><p><b>Categoría:</b> " + item.category +
-                        "</p><p><b>Rating:</b> " + item.rating.rate + " (" + item.rating.count +
-                        " votos)</p></div>");
-        $(".aside-container").html(detalle);
-        
-        detalle.find('.close').click(function() {
-          $(".aside-container").empty();
-        });
-      });  
-      $("#ventas_es").append(card);
+        $("#ventas_es").append(card);
+      });
     });
   });
-});
